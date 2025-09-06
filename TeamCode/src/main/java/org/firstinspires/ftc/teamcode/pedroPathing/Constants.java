a7c0bebcda14ef5f9daae4dfb36fa2ce04a80e7a
eversonli <ammr.noob@gmail.com>
2025-09-06 10:11:03 -0700

    added 3 odometry pod things, still have to resolve errors and tune

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Constants {
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rf")
+            .rightRearMotorName("rr")
+            .leftRearMotorName("lr")
+            .leftFrontMotorName("lf")
+
+
+
+            .fRightWheelDirection(DcMotor.Direction.REVERSE)
+            .fLeftWheelDirection(DcMotor.Direction.FORWARD)
+            .bRightWheelDirection(DcMotor.Direction.REVERSE)
+            .bLeftWheelDirection(DcMotor.Direction.FORWARD)
+    public static FollowerConstants followerConstants = new FollowerConstants()
+        .mass(7.5);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static ThreeWheelConstants localizerConstants = new ThreeWheelConstants()
+            .forwardTicksToInches(.001989436789)
+            .strafeTicksToInches(.001989436789)
+            .turnTicksToInches(.001989436789)
+            .leftPodY(1)
+            .rightPodY(-1)
+            .strafePodX(-2.5)
+            //odometry pod offsets(inches
+            .leftEncoder_HardwareMapName("leftFront")
+            .rightEncoder_HardwareMapName("rightRear")
+            .strafeEncoder_HardwareMapName("rightFront")
+            .leftEncoderDirection(Encoder.FORWARD)
+            .rightEncoderDirection(Encoder.FORWARD)
+            .strafeEncoderDirection(Encoder.FORWARD);
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .threeWheelLocalizer(localizerConstants)
+                .build();
+    }
+}
